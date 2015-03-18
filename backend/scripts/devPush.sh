cde14abe83bc987a2423ca422cdc14f6109e87e4
Derek Parham <derek@duffytech.co>
2015-03-18 18:04:31 +0000

    Update dev scripts

--- backend/scripts/devPush.sh
+++ backend/scripts/devPush.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo su ubuntu <<EOF
-'EOF'
 cd /home/ubuntu/dev/keeper/
 git fetch
 git rebase origin/master
@@ -9,5 +8,8 @@ EOF
 echo "Restarting scripts..."
 sudo stop keeper-celery
 sudo start keeper-celery
+sudo stop keeper-node
+sudo start keeper-node
 
 initctl list | grep duffy-celery
+initctl list | grep duffy-node
